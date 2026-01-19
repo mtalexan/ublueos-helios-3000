b57fc793d2390cb15aeb98cc155eef123467118d
mtalexan <github@trackit.fe80.email>
2026-01-19 14:44:50 -0700

    Update actions and resync with upstream ublueos-image-template

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ popd
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install KDE extras
 dnf5 install -y \
